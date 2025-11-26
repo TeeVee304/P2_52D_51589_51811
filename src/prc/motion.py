540c2595b4776b9892489006402333631a124438
Bruno Pereira <brunonmoura@gmail.com>
2025-11-26 17:00:03 +0000

    Commit

---
+++ src/prc/motion.py
@@ -0,0 +1,7 @@
+import cv2 as cv
+
+class Detector:
+    """Classe responsável pela estimativa e manutenção do modelo de fundo numa sequência de vídeo."""
+
+    def estimate(self, frame):
+        """"""
